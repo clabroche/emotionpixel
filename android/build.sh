c5fd988ba9dfdf2f84852d0608b3a42d6e9904ce
clabroche <corentinlabroche@gmail.com>
2022-01-09 19:53:17 +0100

    fix(deploy): fix capacitor

--- android/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ..
-npx cap sync
-cd android
-./gradlew assembleRelease
-apksigner sign --ks-pass file:keystorepass --ks ~/keystore.jks  --out  ../server/public/emotionpixel.apk ./app/build/outputs/apk/release/app-release-unsigned.apk
